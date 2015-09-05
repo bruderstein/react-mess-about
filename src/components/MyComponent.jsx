c095265d0876c6ec405297ad5ea5905aac025f38
Dave Brotherstone <davegb@pobox.com>
2015-09-05 21:30:40 +0200

    Button clicker

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -3,10 +3,25 @@ import React, { Component } from 'react';
 
 export default class MyComponent extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            count: 0
+        };
+
+        this.onClick = this.onClick.bind(this);
+    }
+
+    onClick() {
+        this.setState({
+            count: this.state.count + 1
+        });
+    }
+
     render() {
 
         return (
-            <div>My first react component</div>
+            <button onClick={this.onClick}>Button was clicked {this.state.count} times</button>
         );
     }
 }
